09abaf089fb8145087d933121151e24d06adc290
DenzelGitonga007 <gitongadenzel@gmail.com>
2022-09-13 23:02:50 +0300

    Users page design

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,4 +1,45 @@
 <!-- Call the layouts -->
 <x-app-layout>
-    <h4>Hello Users</h4>
+    <!-- The users crud -->
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-9">
+                <div class="card">
+                    <div class="card-header"> <h4 style="float: left;">Add User</h4> <a href="#" style="float: right;" class="btn btn-success"> <i class="fa fa-plus"></i> Add New User</a> </div>
+                        <div class="card-body">
+                            <!-- The users crud table -->
+                            <table class="table table-bordered table-left">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Name</th>
+                                        <th>Email</th>
+                                        <th>Phone</th>
+                                        <th>Role</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                </div>
+            </div>
+
+            <div class="col-md-3">
+                <div class="card">
+                    <div class="card-header"> <h4>Search User</h4> </div>
+                        <div class="card-body">
+                            ....
+                        </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </x-app-layout>
