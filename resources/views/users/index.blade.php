c3442158c52cac84da52f801c8439dc9874288d2
DenzelGitonga007 <gitongadenzel@gmail.com>
2022-09-13 23:32:03 +0300

    The modal for adding the user(s)

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -5,7 +5,11 @@
         <div class="row">
             <div class="col-md-9">
                 <div class="card">
-                    <div class="card-header"> <h4 style="float: left;">Add User</h4> <a href="#" style="float: right;" class="btn btn-success"> <i class="fa fa-plus"></i> Add New User</a> </div>
+                    <div class="card-header"> 
+                        <h4 style="float: left;">Add User</h4> 
+                        <a href="#" style="float: right;" class="btn btn-success" data-toggle="modal" data-target="#addUser"> 
+                        <i class="fa fa-plus"></i> Add New User</a> 
+                    </div>
                         <div class="card-body">
                             <!-- The users crud table -->
                             <table class="table table-bordered table-left">
@@ -42,4 +46,35 @@
             </div>
         </div>
     </div>
+
+    <!-- Modal to add new user -->
+    <!-- Modal -->
+    <div class="modal right fade" id="addUser" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="addUserLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title" id="addUserLabel">Add User</h4>
+                    <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <!-- Form for adding a new user -->
+                    <form action=""></form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Style for the modal -->
+    <style>
+        .modal.right .modal-dialog {
+            top: 0;
+            right: 0;
+            margin-right: 20vh;
+        }
+
+        .modal.fade:not(.in).right .modal-dialog {
+            -webkit-transform: translate3d(25%, 0, 0);
+            transform: translate(25%, 0, 0);
+        }
+    </style>
 </x-app-layout>
