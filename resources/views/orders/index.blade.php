5b09492bc714c11a07c3b71363c0cb659e945e03
DenzelGitonga007 <gitongadenzel@gmail.com>
2022-09-22 17:56:05 +0300

    Multiplying the products prices and the wuantities

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -51,9 +51,9 @@
                                         <!-- Product name -->
                                         <td>
                                             <select name="product_id" id="product_id" class="form-select form-control product_id" aria-label="Default select example">
+                                                <option selected>Select product</option>
                                                 @foreach ($products as $product)
-                                                    <option selected>Select product</option>
-                                                    <option value="{{ $product->id }}">{{ $product->product_name }}</option>
+                                                    <option data-price="{{ $product->price }}" value="{{ $product->id }}">{{ $product->product_name }}</option>
                                                 @endforeach
                                             </select>
                                         </td>
@@ -65,15 +65,15 @@
                                         </td> -->
                                         <!-- Quantity -->
                                         <td>
-                                            <input type="number" name="quantity[]" id="quantity" class="form-control">
+                                            <input type="number" name="quantity[]" id="quantity" class="form-control quantity">
                                         </td>
                                         <!-- Price -->
                                         <td>
-                                            <input type="number" name="price[]" id="price" class="form-control">
+                                            <input type="number" name="price[]" id="price" class="form-control price">
                                         </td>
                                         <!-- Total -->
                                         <td>
-                                            <input type="number" name="total_amount[]" id="total_amount" class="form-control" disabled>
+                                            <input type="number" name="total_amount[]" id="total_amount" class="form-control total_amount" disabled>
                                         </td>
                                         <!-- To delete the product from the order table -->
                                         <td>
@@ -95,7 +95,7 @@
             <div class="col-md-3">
                 <div class="card">
                     <div class="card-header"> 
-                        <h4>Total 00.00</h4> 
+                        <h4>Total <b class="total"> 00.00 </b></h4> 
                     </div>
                         <div class="card-body">
                             ....
@@ -190,11 +190,11 @@
                 var numberofrow = ($('.addMoreProduct tr').length - 0) + 1;
                 // Adding products onto the order table
                 var tr = '<tr><td class"no"">' + numberofrow + '</td>' +
-                        '<td><select class="form-control product_id" name="product_id[]" >' + product + 
+                        '<td><select class="form-control form-select product_id" name="product_id[]" >' + product + 
                         ' </select></td>' +
-                        '<td> <input type="number" name="quatity[]" class="form-control"></td>'+
-                        '<td> <input type="number" name="price[]" class="form-control"></td>'+
-                        '<td> <input type="number" name="total_amount[]" class="form-control"></td>'+
+                        '<td> <input type="number" name="quatity[]" class="form-control quantity"></td>'+
+                        '<td> <input type="number" name="price[]" class="form-control price"></td>'+
+                        '<td> <input type="number" name="total_amount[]" class="form-control total_amount"></td>'+
                         // To delete the added products
                         '<td> <a class="btn btn-sm btn-outline-danger delete rounded-circle"><i class="fa fa-times-circle"></a></td>';
                 // Append onto the table
@@ -204,6 +204,41 @@
             $('.addMoreProduct').delegate('.delete', 'click', function(){
                 $(this).parent().parent().remove();
             })
+
+            // Doing the computations of the prices and quantities
+            function totalAmount() {
+                var total = 0;
+                $('.total_amount').each(function(i, e){
+                    var amount = $(this).val() - 0;
+                    total += amount;
+                });
+
+                $('.total').html(total); // class total is for the total div on the right
+            }
+
+            $('.addMoreProduct').delegate('.product_id', 'change', function(){
+                var tr = $(this).parent().parent();
+                var price = tr.find('.product_id option:selected').attr('data-price');
+                tr.find('.price').val(price);
+                var quantity = tr.find('.quantity').val() - 0;
+                var price = tr.find('.price').val() - 0;
+                var total_amount = (quantity * price);
+                tr.find('.total_amount').val(total_amount);
+                // Call the function now
+                totalAmount();
+            });
+
+            $('.addMoreProduct').delegate('.quantity, .discount', 'keyup', function(){
+                var tr = $(this).parent().parent();
+                var quantity = tr.find('.quantity').val() - 0;
+                var price = tr.find('.price').val() - 0;
+                var total_amount = (quantity * price);
+                tr.find('.total_amount').val(total_amount);
+                // Call the function now
+                totalAmount();
+                
+            })
+
 
         </script>
     @endsection
